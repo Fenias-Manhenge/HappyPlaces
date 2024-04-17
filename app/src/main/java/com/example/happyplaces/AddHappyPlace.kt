eaeb37731839836da3eab3d86fafc84febaf9971
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2024-04-17 14:23:46 +0200

    Finished the Permission and dialogs logic!

--- app/src/main/java/com/example/happyplaces/AddHappyPlace.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlace.kt
@@ -1,30 +1,21 @@
 package com.example.happyplaces
 
-import android.Manifest
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.view.MenuItem
 import android.view.View
 import android.widget.PopupMenu
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia.Companion.isPhotoPickerAvailable
 import androidx.annotation.MenuRes
-import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.happyplaces.databinding.AddHappyPlaceBinding
 import com.google.android.material.datepicker.MaterialDatePicker
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import java.io.Reader
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -33,34 +24,6 @@ import java.util.TimeZone
 class AddHappyPlace : AppCompatActivity() {
 
     private val binding by lazy { AddHappyPlaceBinding.inflate(layoutInflater) }
-
-    companion object{
-        private var PERMISSION_DENIED_BEFORE = false
-
-        private val CAMERA_PERMISSIONS =
-            mutableListOf(
-                Manifest.permission.CAMERA
-            ).apply {
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q){
-                    addAll(arrayOf(
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                            Manifest.permission.READ_EXTERNAL_STORAGE
-                        ))
-                }else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2){
-                    add(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
-                } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU){
-                    add(Manifest.permission.READ_MEDIA_IMAGES)
-                } else
-                    addAll(arrayOf(
-                        Manifest.permission.READ_MEDIA_IMAGES,
-                        Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
-                        ))
-            }.toTypedArray()
-
-        private fun hasPermissions(context: Context) = CAMERA_PERMISSIONS.all { permissions ->
-            ContextCompat.checkSelfPermission(context, permissions) == PackageManager.PERMISSION_GRANTED
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -77,7 +40,7 @@ class AddHappyPlace : AppCompatActivity() {
 
         binding.tbAddHappyPlace.setNavigationOnClickListener {
             this.finish()
-            TODO("Add callBack method")
+            startActivity(Intent(this, MainActivity::class.java))
         }
 
         onBackPressedDispatcher.addCallback(
@@ -122,11 +85,14 @@ class AddHappyPlace : AppCompatActivity() {
         popupMenu.setOnMenuItemClickListener { menuItem: MenuItem ->
             when(menuItem.itemId){
                 R.id.menuGallery ->{
-                    Toast.makeText(this, "Gallery selected", Toast.LENGTH_LONG).show()
+                    if (!hasStoragePermission(baseContext))
+                        askStoragePermission.launch(STORAGE_PERMISSIONS)
+                    else
+                        photoPicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
                 }
                 R.id.menuCamera ->{
-                    if (!hasPermissions(baseContext))
-                        askPermissions.launch(CAMERA_PERMISSIONS)
+                    if (!hasCameraPermissions(baseContext))
+                        askCameraPermissions.launch(CAMERA_PERMISSIONS)
                     else
                         camera.launch(null)
                 }
@@ -136,11 +102,36 @@ class AddHappyPlace : AppCompatActivity() {
         popupMenu.show()
     }
 
+    private val askStoragePermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ permissions ->
+        var permissionGranted = true
+
+        permissions.entries.forEach{
+            if (it.key in STORAGE_PERMISSIONS && !it.value)
+                permissionGranted = false
+        }
+
+        if (!permissionGranted){
+            if (!PERMISSION_DENIED_BEFORE)
+                showStoragePermissionRationalDialog()
+            else
+                showSettingsRationalDialog()
+        }else {
+            if (isPhotoPickerAvailable(this))
+                photoPicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+    }
+
+
+    private val photoPicker = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){ uri ->
+        if (uri != null)
+            binding.ivAddImage.setImageURI(uri)
+    }
+
     private val camera = registerForActivityResult(ActivityResultContracts.TakePicturePreview()){ bitmap ->
-            binding.ivAddImage.setImageBitmap(bitmap)
+        binding.ivAddImage.setImageBitmap(bitmap)
     }
     
-    private val askPermissions = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ permissions->
+    private val askCameraPermissions = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ permissions->
         var permissionGranted = true
 
         permissions.entries.forEach {
@@ -150,43 +141,22 @@ class AddHappyPlace : AppCompatActivity() {
 
         if (!permissionGranted){
             if (!PERMISSION_DENIED_BEFORE) {
-                rationalDialog()
+                showCameraPermissionRationalDialog()
             } else
-                rationalDialogSettings()
+                showSettingsRationalDialog()
         } else
             camera.launch(null)
     }
 
-    private fun rationalDialog(){
-        MaterialAlertDialogBuilder(this)
-            .setCancelable(false)
-            .setTitle(resources.getString(R.string.warning))
-            .setMessage(resources.getString(R.string.warning_message))
-            .setPositiveButton("Okay!"){ _, _ ->
-                PERMISSION_DENIED_BEFORE = true
-                askPermissions.launch(CAMERA_PERMISSIONS)
-            }
-            .setNegativeButton("Cancel"){ dialog, _ ->
-                PERMISSION_DENIED_BEFORE = true
-                dialog.dismiss()
-            }
-            .show()
+    private fun showCameraPermissionRationalDialog(){
+        ShowingDialogs.rationalDialogCamera(this, askCameraPermissions)
     }
 
-    private fun rationalDialogSettings(){
-        MaterialAlertDialogBuilder(this)
-            .setCancelable(false)
-            .setTitle(resources.getString(R.string.warning_disabled))
-            .setMessage(resources.getString(R.string.warning_message_disabled))
-            .setPositiveButton("Go to settings!"){ _, _ ->
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                val uri = Uri.fromParts("package", packageName, null)
-                intent.data = uri
-                startActivity(intent)
-            }
-            .setNegativeButton("Cancel"){ dialog, _ ->
-                dialog.dismiss()
-            }
-            .show()
+    private fun showSettingsRationalDialog(){
+        ShowingDialogs.rationalDialogSettings(this)
+    }
+
+    private fun showStoragePermissionRationalDialog(){
+        ShowingDialogs.rationalDialogStorage(this, askStoragePermission)
     }
 }
